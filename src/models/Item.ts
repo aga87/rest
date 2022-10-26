8abbe11e83d61bd0a215402d61e912c1c612304c
aga87 <aga.labonarska@outlook.com>
2022-10-26 17:39:22 +0200

    Update Item schema - make desc optional + add timestamps

--- src/models/Item.ts
+++ src/models/Item.ts
@@ -1,16 +1,21 @@
 import { Schema, model } from 'mongoose';
 
-const itemSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    maxLength: 100
+const itemSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxLength: 100,
+      trim: true
+    },
+    description: {
+      type: String,
+      default: null,
+      maxLength: 5000,
+      trim: true
+    }
   },
-  description: {
-    type: String,
-    required: true,
-    maxLength: 5000
-  }
-});
+  { timestamps: true }
+);
 
 export const Item = model('Item', itemSchema);
