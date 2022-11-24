835dbf4a545a8afa82eeb55b4a567492e2a1ea8b
aga87 <aga.labonarska@outlook.com>
2022-11-24 18:35:54 +0100

    Add integration test for verification token renewal endpoint

--- src/tests/integration/security/email-verification.test.ts
+++ src/tests/integration/security/email-verification.test.ts
@@ -4,7 +4,7 @@ import { app } from '../../../app';
 import { User } from '../../../models/User';
 import { Token } from '../../../models/Token';
 
-describe.only('/api/v1/security/email-verification', () => {
+describe('/api/v1/security/email-verification', () => {
   beforeAll(async () => {
     await mongoose.connect(process.env.MONGO_URI_TEST as string);
   });
@@ -12,6 +12,7 @@ describe.only('/api/v1/security/email-verification', () => {
   afterEach(async () => {
     // Clean up the database
     await User.deleteMany({});
+    await Token.deleteMany({});
   });
 
   afterAll(() => {
@@ -19,12 +20,12 @@ describe.only('/api/v1/security/email-verification', () => {
   });
 
   describe('POST /', () => {
-    let token: any;
+    let reqBody: any;
 
     const act = async () =>
       await request(app)
         .post('/api/v1/security/email-verification')
-        .send(token);
+        .send(reqBody);
 
     beforeEach(async () => {
       // Happy path
@@ -42,19 +43,19 @@ describe.only('/api/v1/security/email-verification', () => {
 
       await verificationToken.save();
 
-      token = {
+      reqBody = {
         token: verificationToken.token
       };
     });
 
     it('should return 400 if token is missing', async () => {
-      token = {};
+      reqBody = {};
       const res = await act();
       expect(res.status).toBe(400);
     });
 
     it('should return 401 if token is invalid or expired', async () => {
-      token = {
+      reqBody = {
         token: 'a'
       };
       const res = await act();
@@ -76,13 +77,77 @@ describe.only('/api/v1/security/email-verification', () => {
 
       it('should remove the token from the DB', async () => {
         await act();
-        const tokenInDB = await Token.findOne({ token: token.token });
+        const tokenInDB = await Token.findOne({ token: reqBody.token });
         expect(tokenInDB).toBeNull();
       });
 
       it('should return 200 status code', async () => {
         const res = await act();
         expect(res.status).toBe(200);
+      });
+    });
+  });
+
+  describe('POST /new-token', () => {
+    let reqBody: any;
+    let token: string;
+    let userId: mongoose.Types.ObjectId;
+
+    const act = async () =>
+      await request(app)
+        .post('/api/v1/security/email-verification/new-token')
+        .send(reqBody);
+
+    beforeEach(async () => {
+      const user = new User({
+        name: 'a',
+        email: 'a@a.com',
+        password: '123aA%'
+      });
+
+      await user.save();
+
+      const verificationToken = new Token({
+        userId: user._id
+      });
+
+      await verificationToken.save();
+
+      reqBody = {
+        email: user.email
+      };
+
+      token = verificationToken.token;
+      userId = user._id;
+    });
+
+    it('should return 400 if email is missing', async () => {
+      reqBody = {};
+      const res = await act();
+      expect(res.status).toBe(400);
+    });
+
+    describe('if email is valid / SUCCESS', () => {
+      it('should remove the original from the DB', async () => {
+        await act();
+        const oldToken = await Token.findOne({ token });
+        expect(oldToken).toBeNull();
+      });
+
+      it('should generate and save a new token for that user in the DB', async () => {
+        await act();
+        const newToken = await Token.findOne({ userId });
+        expect(newToken).not.toBeNull();
+      });
+
+      it('should return 200 status code', async () => {
+        const res = await act();
+        expect(res.status).toBe(200);
+      });
+
+      it('should return a message in the response body', async () => {
+        const res = await act();
+        expect(res.body.msg).toBeTruthy();
       });
     });
   });
