2f40efb12202c54edfeb47e49edc28bfd357e5f6
aga87 <aga.labonarska@outlook.com>
2022-11-24 19:46:06 +0100

    Implement endpoint to request password reset token

---
+++ src/controllers/security/forgotten-password.ts
@@ -0,0 +1,70 @@
+import { RequestHandler } from 'express';
+import Joi from 'joi';
+import { User } from '../../models/User';
+import { Token } from '../../models/Token';
+import { getHATEOAS, sendEmail, validateSchema } from '../../utils';
+
+export const generatePasswordResetToken: RequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  const error = validateSchema({
+    reqSchema: req.body,
+    validSchema: Joi.object({
+      email: Joi.string().required().email()
+    })
+  });
+
+  if (error) return res.status(400).send(error);
+
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user && !user.isVerified) {
+      await sendEmail({
+        to: user.email,
+        subject: 'Password reset',
+        html: `<p>Please verify your email address before requesting a password change.</p>`
+      });
+    }
+
+    if (user?.isVerified) {
+      // Remove old token if it exists
+      await Token.findOneAndDelete({ userId: user._id });
+
+      // Generate and save new token
+      const token = new Token({
+        userId: user._id
+      });
+      await token.save();
+
+      // Send the token by email
+      await sendEmail({
+        to: user.email,
+        subject: 'Password reset',
+        html: `<p>Password reset token: <br /><br /><b>${token.token}</b><br /><br />The token can only be used once and will expire in 30 minutes.</p>`
+      });
+    }
+
+    res.send({
+      msg: `If a matching account exists, a password reset token has been sent to ${req.body.email}`,
+      _links: getHATEOAS(req.originalUrl, [
+        {
+          href: `${process.env.BASE_URL}/api/v1/security/forgotten-password/reset`,
+          rel: 'reset password'
+        },
+        {
+          href: `${process.env.BASE_URL}/api/v1/auth`,
+          rel: 'login'
+        },
+        {
+          href: `${process.env.BASE_URL}/api/v1/security/email-verification`,
+          rel: 'email verification'
+        }
+      ])
+    });
+  } catch (err) {
+    next(err);
+  }
+};
--- src/routes/security.ts
+++ src/routes/security.ts
@@ -3,11 +3,14 @@ import {
   renewVerificationToken,
   verifyUser
 } from '../controllers/security/email-verification';
+import { generatePasswordResetToken } from '../controllers/security/forgotten-password';
 
 const router = Router();
 
 router.post('/email-verification', verifyUser);
 
 router.post('/email-verification/new-token', renewVerificationToken);
+
+router.post('/forgotten-password', generatePasswordResetToken);
 
 export { router as security };
