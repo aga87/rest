0c6b307d6f33859695d98908efdaa1c6f03cd336
aga87 <aga.labonarska@outlook.com>
2022-11-24 18:31:57 +0100

    Implement endpoint to renew verification token

--- src/controllers/security/email-verification.ts
+++ src/controllers/security/email-verification.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from 'express';
 import Joi from 'joi';
 import { User } from '../../models/User';
 import { Token } from '../../models/Token';
-import { getHATEOAS, validateSchema } from '../../utils';
+import { getHATEOAS, sendEmail, validateSchema } from '../../utils';
 
 export const verifyUser: RequestHandler = async (req, res, next) => {
   const error = validateSchema({
@@ -55,6 +55,63 @@ export const verifyUser: RequestHandler = async (req, res, next) => {
         {
           href: `${process.env.BASE_URL}/api/v1/auth`,
           rel: 'login'
+        }
+      ])
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const renewVerificationToken: RequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  const error = validateSchema({
+    reqSchema: req.body,
+    validSchema: Joi.object({
+      email: Joi.string().required().email()
+    })
+  });
+
+  if (error) return res.status(400).send(error);
+
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user?.isVerified) {
+      await sendEmail({
+        to: user.email,
+        subject: 'Welcome',
+        html: `<p>Your account had already been verified. You can proceed to login.</p>`
+      });
+    }
+
+    if (user && !user.isVerified) {
+      // Delete the old token
+      await Token.findOneAndDelete({ userId: user._id });
+
+      // Generate and save new token
+      const newToken = new Token({
+        userId: user._id
+      });
+      await newToken.save();
+
+      // Send the token by email
+      await sendEmail({
+        to: user.email,
+        subject: 'Welcome',
+        html: `<p>Verification token: <br /><br /><b>${newToken.token}</b><br /><br />The token will expire in 30 minutes.</p>`
+      });
+    }
+
+    res.send({
+      msg: `If a matching account exists, new verification token has been sent to ${req.body.email}`,
+      _links: getHATEOAS(req.originalUrl, [
+        {
+          href: `${process.env.BASE_URL}/api/v1/security/email-verification`,
+          rel: 'email verification'
         }
       ])
     });
--- src/routes/security.ts
+++ src/routes/security.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
-import { verifyUser } from '../controllers/security/email-verification';
+import {
+  renewVerificationToken,
+  verifyUser
+} from '../controllers/security/email-verification';
 
 const router = Router();
 
 router.post('/email-verification', verifyUser);
+
+router.post('/email-verification/new-token', renewVerificationToken);
 
 export { router as security };
