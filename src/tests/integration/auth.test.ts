0c7743279987e296b71f5a50885e7bdfa1128e81
aga87 <aga.labonarska@outlook.com>
2022-12-02 17:21:51 +0100

    Add integration test for refresh token endpoint

--- src/tests/integration/auth.test.ts
+++ src/tests/integration/auth.test.ts
@@ -82,4 +82,64 @@ describe('/api/v1/auth', () => {
       });
     });
   });
+
+  describe('POST /refresh-token', () => {
+    let reqBody: any;
+    let userId: mongoose.Types.ObjectId;
+
+    const act = async () =>
+      await request(app).post('/api/v1/auth/refresh-token').send(reqBody);
+
+    beforeEach(async () => {
+      // Happy path
+      const user = await new User({
+        name: 'a',
+        email: 'a@a.com',
+        password: '123aA%',
+        isVerified: true
+      }).save();
+
+      userId = user._id;
+
+      const refreshToken = user.generateRefreshToken();
+
+      await new Token({
+        token: refreshToken,
+        userId,
+        type: 'refresh'
+      }).save();
+
+      reqBody = { refreshToken };
+    });
+
+    it('should return 400 if refresh token is missing', async () => {
+      reqBody = {};
+      const res = await act();
+      expect(res.status).toBe(400);
+    });
+
+    it('should return 401 if refresh token is invalid', async () => {
+      reqBody.refreshToken = 'a';
+      const res = await act();
+      expect(res.status).toBe(401);
+    });
+
+    it('should return 404 if refresh token is valid but user does not exist', async () => {
+      await User.findByIdAndDelete(userId);
+      const res = await act();
+      expect(res.status).toBe(404);
+    });
+
+    describe('refresh token is valid and user is exists / SUCCESS', () => {
+      it('should return access token', async () => {
+        const res = await act();
+        expect(res.body.accessToken).toBeTruthy();
+      });
+
+      it('should return 200 status code', async () => {
+        const res = await act();
+        expect(res.status).toBe(200);
+      });
+    });
+  });
 });
