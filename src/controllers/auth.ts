3438716c36d151efc9b30c990b86594b4436a4af
aga87 <aga.labonarska@outlook.com>
2022-12-02 16:53:47 +0100

    Implement login endpoint

---
+++ src/controllers/auth.ts
@@ -0,0 +1,87 @@
+import { RequestHandler } from 'express';
+import bcrypt from 'bcrypt';
+import { User, loginSchema } from '../models/User';
+import { Token } from '../models/Token';
+import { getHATEOAS, validateSchema } from '../utils';
+
+export const login: RequestHandler = async (req, res, next) => {
+  const error = validateSchema({
+    reqSchema: req.body,
+    validSchema: loginSchema
+  });
+  if (error) return res.status(400).send(error);
+
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) return res.status(400).send('Invalid email or password.');
+
+    // Compare plain text password with hashed password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(400).send('Invalid email or password.');
+
+    if (!user.isVerified)
+      return res.status(401).send({
+        msg: 'Please verify your email address before logging in.',
+        _links: getHATEOAS(req.originalUrl, [
+          {
+            href: `${process.env.BASE_URL}/api/v1/security/email-verification`,
+            rel: 'email verification'
+          },
+          {
+            href: `${process.env.BASE_URL}/api/v1/security/email-verification/new-token`,
+            rel: 'new token'
+          }
+        ])
+      });
+
+    const accessToken = user.generateAccessToken();
+
+    // Remove old refresh token from the DB (this way we don't need the logout endpoint TODO: is this true?)
+    await Token.findOneAndDelete({ userId: user._id, type: 'refresh' });
+
+    // Generate new refresh token
+    const refreshToken = user.generateRefreshToken();
+
+    // Store new refresh token in the DB (so that it can be revoked)
+    await new Token({
+      userId: user._id,
+      token: refreshToken,
+      type: 'refresh'
+    }).save();
+
+    res.send({
+      user: {
+        name: user.name,
+        email: user.email,
+        // https://www.rfc-editor.org/rfc/rfc6749#section-5.1
+        token: {
+          accessToken,
+          tokenType: 'Bearer',
+          expiresIn: '15m',
+          refreshToken
+        }
+      },
+      _links: getHATEOAS(req.originalUrl, [
+        {
+          href: `${process.env.BASE_URL}/api/v1/auth/refresh-token`,
+          rel: 'refresh token'
+        },
+        {
+          href: `${process.env.BASE_URL}/api/v1/items`,
+          rel: 'items'
+        },
+        {
+          href: `${process.env.BASE_URL}/api/v1/tags`,
+          rel: 'tags'
+        }
+      ])
+    });
+  } catch (err) {
+    next(err);
+  }
+};
--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -1,4 +1,5 @@
 import express, { Application } from 'express';
+import { auth } from '../routes/auth';
 import { items } from '../routes/items';
 import { tags } from '../routes/tags';
 import { users } from '../routes/users';
@@ -9,9 +10,10 @@ import { morganMiddleware } from '../middleware/morgan';
 export const routes = (app: Application) => {
   app.use(express.json()); // middleware for parsing application/json
   app.use(morganMiddleware); // middleware for logging HTTP requests
+  app.use('/api/v1/security', security);
+  app.use('/api/v1/auth', auth);
   app.use('/api/v1/items', items);
   app.use('/api/v1/tags', tags);
   app.use('/api/v1/users', users);
-  app.use('/api/v1/security', security);
   app.use(errorMiddleware);
 };
