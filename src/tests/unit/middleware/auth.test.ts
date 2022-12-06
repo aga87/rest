0a615b58466ee7e8e2e9af7aa18fbe53e526ccb1
aga87 <aga.labonarska@outlook.com>
2022-12-06 17:38:04 +0100

    Update token payloads in unit tests for auth middleware

--- src/tests/unit/middleware/auth.test.ts
+++ src/tests/unit/middleware/auth.test.ts
@@ -7,7 +7,8 @@ describe('auth middleware', () => {
   describe('If JWT is valid', () => {
     // Synchronous setup - no need for beforeAll
     const user = {
-      _id: new mongoose.Types.ObjectId().toHexString()
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true
     };
     const token = new User(user).generateAccessToken();
 
@@ -23,11 +24,14 @@ describe('auth middleware', () => {
     authMiddleware(req, res, next);
 
     it('should populate req.user with the payload of a valid JWT', () => {
-      expect(req.user).toMatchObject({ userId: user._id });
+      expect(req.user).toMatchObject({
+        userId: user._id,
+        isAdmin: user.isAdmin
+      });
     });
 
     it('should pass control to the next middleware/ handler', () => {
-      expect(next).toHaveBeenCalledTimes(1); // ??
+      expect(next).toHaveBeenCalledTimes(1);
     });
   });
 });
